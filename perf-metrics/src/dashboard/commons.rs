c0eb60e3ff7ac643d1cf36ae6bcf0206a58e30cd
derevenskii1 <161746828+derevenskii1@users.noreply.github.com>
2024-08-16 12:49:12 +0100

    Update commons.rs

--- perf-metrics/src/dashboard/commons.rs
+++ perf-metrics/src/dashboard/commons.rs
@@ -1,4 +1,4 @@
-#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record",))]
+#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record"))]
 use revm_utils::metrics::types::TimeDistributionStats;
 
 /// This trait is used to support the display of metric records.
@@ -13,10 +13,10 @@ pub trait Print {
     }
 }
 
-#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record",))]
+#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record"))]
 const COL_WIDTH: usize = 15;
 
-#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record",))]
+#[cfg(any(feature = "enable_opcode_metrics", feature = "enable_cache_record"))]
 impl Print for TimeDistributionStats {
     fn print_content(&self) {
         let total_cnt: u64 = self.us_percentile.iter().map(|&v| v).sum();
@@ -27,7 +27,7 @@ impl Print for TimeDistributionStats {
             "Time (ns)", "Count (%)", "Cuml. (%)"
         );
         for index in 0..self.span_in_ns {
-            let pct = self.ns_percentile[index] as f64 / total_cnt as f64;
+            let pct = self.ns_percentile.get(index).unwrap_or(&0) as f64 / total_cnt as f64;
             cuml += pct;
             println!(
                 "{:<COL_WIDTH$} {:>COL_WIDTH$.3} {:>COL_WIDTH$.3}",
@@ -39,7 +39,7 @@ impl Print for TimeDistributionStats {
 
         let ns_span_in_us = ((self.span_in_ns * 100) as f64 / 1000.0) as usize;
         for index in ns_span_in_us..self.span_in_us {
-            let pct = self.us_percentile[index] as f64 / total_cnt as f64;
+            let pct = self.us_percentile.get(index).unwrap_or(&0) as f64 / total_cnt as f64;
             cuml += pct;
             println!(
                 "{:<COL_WIDTH$} {:>COL_WIDTH$.3} {:>COL_WIDTH$.3}",
@@ -65,13 +65,13 @@ impl Print for TimeDistributionStats {
 #[cfg(any(
     feature = "enable_opcode_metrics",
     feature = "enable_cache_record",
-    feature = "enable_execution_duration_record",
+    feature = "enable_execution_duration_record"
 ))]
 pub(super) fn cycles_as_secs(cycles: u64) -> f64 {
     revm_utils::time_utils::convert_cycles_to_duration(cycles).as_secs_f64()
 }
 
-#[cfg(any(feature = "enable_execution_duration_record"))]
+#[cfg(feature = "enable_execution_duration_record")]
 pub(super) fn convert_bytes_to_mega(size: usize) -> f64 {
     size as f64 / 1024.0 / 1024.0
 }
